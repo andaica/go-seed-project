393185f79adc79a1f5155a7db4a4ec9fa5181c99
Thanh An <annt@ows.vn>
2019-08-28 15:15:47 +0700

    add method http for request middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	log "../logger"
+)
 
 type mFunc func(http.HandlerFunc) http.HandlerFunc
 
@@ -13,10 +17,42 @@ func (h Handler) Use(m mFunc) (newHandler Handler) {
 	return
 }
 
-func (h Handler) GetHandler() http.HandlerFunc {
-	return h.handler
-}
-
 func NewHandler(handler http.HandlerFunc) Handler {
 	return Handler{handler: handler}
+}
+
+func (h Handler) Get() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			log.Error("Request method incorrect")
+		}
+		h.handler(w, r)
+	}
+}
+
+func (h Handler) Post() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			log.Error("Request method incorrect")
+		}
+		h.handler(w, r)
+	}
+}
+
+func (h Handler) Put() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			log.Error("Request method incorrect")
+		}
+		h.handler(w, r)
+	}
+}
+
+func (h Handler) Delete() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			log.Error("Request method incorrect")
+		}
+		h.handler(w, r)
+	}
 }
